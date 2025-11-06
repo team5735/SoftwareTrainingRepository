7b7bae887e3bc2a83ca832b46124c4e5da1211ee
Aaryan Raj Nair <aaryan_raj@student.waylandps.org>
2025-11-05 20:21:49 -0500

    67 41

--- src/main/java/frc/robot/subsystems/MotorSubsystem.java
+++ src/main/java/frc/robot/subsystems/MotorSubsystem.java
@@ -1,5 +1,8 @@
 package frc.robot.subsystems;
-
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
@@ -14,32 +17,29 @@ import frc.robot.constants.MotorSubsystemConstants;
 public class MotorSubsystem extends SubsystemBase {
     private final SparkMax sparkMax_pull = new SparkMax(Constants.mot, com.revrobotics.spark.SparkLowLevel.MotorType.kBrushless);
     private final SparkBaseConfig sparkConfig = new SparkMaxConfig().inverted(true);
+    private final SparkMax sparkMax_pull2 = new SparkMax(Constants.mot2, com.revrobotics.spark.SparkLowLevel.MotorType.kBrushless);
     public MotorSubsystem() {
         
         sparkMax_pull.configure(sparkConfig, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters);
+        sparkMax_pull2.configure(sparkConfig, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters);
+
     }
 
     public void pull() {
         sparkMax_pull.setVoltage(MotorSubsystemConstants.PULL_VOLTS);
+        sparkMax_pull2.setVoltage(MotorSubsystemConstants.STOP_VOLTS);
+
     }
 
-    public void push() {
-        sparkMax_pull.setVoltage(-MotorSubsystemConstants.PUSH_VOLTS);
-    }
+    // public void push() {
+    //     sparkMax_pull.setVoltage(-MotorSubsystemConstants.PUSH_VOLTS);
+    //     sparkMax_pull2.setVoltage(-MotorSubsystemConstants.PUSH_VOLTS);
+
+    // }
 
     public void stop() {
-        sparkMax_pull.setVoltage(0);
-    }
+        sparkMax_pull.setVoltage(MotorSubsystemConstants.STOP_VOLTS);
+        sparkMax_pull2.setVoltage(MotorSubsystemConstants.PULL_VOLTS);
 
-    public Command pullStopCommand() {
-        return startEnd(() -> pull(), () -> stop());
-    }
-
-    public Command pushStopCommand() {
-        return runOnce(() -> push());
-    }
-
-    public Command stopCommand() {
-        return runOnce(() -> stop());
     }
 }
